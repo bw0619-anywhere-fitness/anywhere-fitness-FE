b328b4bf9bc51c41ca50fc1cbf6d93a4fb7798c8
chrisdalao <chrisdalao@gmail.com>
2019-06-22 16:47:34 -0700

    stores - middleWare - router added to project - routes a WIP

--- anywhere-fitness-fe/src/index.js
+++ anywhere-fitness-fe/src/index.js
@@ -2,5 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+
+    <Provider store={store}>
+        <Router>
+            <Route path="/" component={App} />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);

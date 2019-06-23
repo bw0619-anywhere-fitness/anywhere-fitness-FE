0379178511694fd7b1fa09e29601eb31076c4aaa
chrisdalao <chrisdalao@gmail.com>
2019-06-22 19:41:04 -0700

    register form styled - change handler added

--- anywhere-fitness-fe/src/components/Register.js
+++ anywhere-fitness-fe/src/components/Register.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
+import Loader from "react-loader-spinner";
 
 export class Register extends Component {
+    state = {
+        credentials: {
+            fullname: "",
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
     render() {
         return (
             <div className="register">
                 Register Page
+                <form className="register-form" onSubmit={this.register}>
+                    <input
+                        type="text"
+                        name="fullname"
+                        placeholder="Full Name"
+                        value={this.state.credentials.fullname}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>
+                        {this.props.registering ? (
+                            <Loader type="ThreeDots" color="#1f2a38" height="12" width="26" />
+                        ) : (
+                                "Log in"
+                            )}
+                    </button>
+                </form>
             </div>
         )
     }

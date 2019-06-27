e525f93c60a7b3e319486c90d87a488571eca206
chrisdalao <chrisdalao@gmail.com>
2019-06-26 17:46:00 -0700

    reroute refactor

--- anywhere-fitness-fe/src/components/Instructors/Welcome.js
+++ anywhere-fitness-fe/src/components/Instructors/Welcome.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
 
 export class Welcome extends Component {
+    componentDidMount() {
+        this.props.history.push('/instructor-login');
+    }
     render() {
         return (
             <div className="welcome">
                 <div className="welcome-header">
                     <h1> Welcome to Anywhere Fitness!</h1>
-                    <div className="welcome-links">
-                        <Link to="/instructor-login">Login</Link>
-                        <Link to="/instructor-register">Register</Link>
-                    </div>
                 </div>
             </div>
         )

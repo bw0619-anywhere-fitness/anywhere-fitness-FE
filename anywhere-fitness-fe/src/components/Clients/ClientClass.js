2ad2885f87620dce3f70c24c3cb1002f7f333782
Chris Dalao <chrisdalao@gmail.com>
2019-06-27 18:35:40 -0700

    ALL classes are now rendering on client's home page

---
+++ anywhere-fitness-fe/src/components/Clients/ClientClass.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ClientClass = props => {
+  console.log(props);
+  return (
+    <div className="client-class">
+      <div className="client-class-label">Class</div>
+      <div className="client-class-name">
+        <div className="class-label">class:</div>
+        <div className="class-prop">{props.clientClass.name}</div>
+      </div>
+      <div className="client-class-date">
+        <div className="class-label">date:</div>
+        <div className="class-prop">{props.clientClass.date}</div>
+      </div>
+      <div className="client-class-time">
+        <div className="class-label">time:</div>
+        <div className="class-prop">{props.clientClass.time}</div>
+      </div>
+      <div className="client-class-address">
+        <div className="class-label">address:</div>
+        <div className="class-prop">{props.clientClass.address}</div>
+      </div>
+      <div className="client-class-city">
+        <div className="class-label">city:</div>
+        <div className="class-prop">{props.clientClass.city}</div>
+      </div>
+      <div className="client-class-zipcode">
+        <div className="class-label">zipcode:</div>
+        <div className="class-prop">{props.clientClass.zipcode}</div>
+      </div>
+      <div className="client-class-description">
+        <div className="class-label">description:</div>
+        <div className="class-prop">{props.clientClass.description}</div>
+      </div>
+      <Link to={`/client/class`}>
+        <button className="view-details-btn">View Details</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ClientClass;
--- anywhere-fitness-fe/src/components/Clients/ClientHome.js
+++ anywhere-fitness-fe/src/components/Clients/ClientHome.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { getAllClassesClient } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAllClassesClient } from "../../actions";
+import ClientClass from "./ClientClass";
 
 export class ClientHome extends Component {
-
-    render() {
-        console.log(this.props)
-        return (
-            <div className="client-home">
-                Client Home
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.getAllClassesClient();
+  }
+  render() {
+    return (
+      <div className="client-home">
+        <div className="client-classes">
+          {this.props.clientClasses.map(clientClass => {
+            return (
+              <ClientClass key={clientClass.id} clientClass={clientClass} />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        clientClasses: state.clientHomeReducer.clientClasses,
-        error: state.clientHomeReducer.error
-    }
-}
+  return {
+    clientClasses: state.clientHomeReducer.clientClasses,
+    error: state.clientHomeReducer.error
+  };
+};
 
 export default connect(
-    mapStateToProps,
-    {
-        getAllClassesClient
-    }
-)(ClientHome)
+  mapStateToProps,
+  {
+    getAllClassesClient
+  }
+)(ClientHome);

d3ef84170d1405d2e9d83c56f3321599e1a88551
Chris Dalao <chrisdalao@gmail.com>
2019-06-28 04:29:07 -0700

    deploy debugging

--- anywhere-fitness-fe/src/components/Clients/ClientSignUpClasses.js
+++ anywhere-fitness-fe/src/components/Clients/ClientSignUpClasses.js
@@ -10,7 +10,6 @@ export class ClientSignUpClasses extends Component {
   componentDidMount() {
     this.props.getClassByClientId(cookie.load("client")[0]["id"]);
   }
-
   render() {
     return (
       <div className="client-signup-classes">
